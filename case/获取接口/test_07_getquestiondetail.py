ad55dd4a484f78b154fded1d96e619c97786c017
ykl <1959733095@qq.com>
2020-09-18 23:25:37 +0800

    ok

---
+++ case/获取接口/test_07_getquestiondetail.py
@@ -0,0 +1,69 @@
+import pytest
+import requests
+import os,sys
+sys.path.append(os.getcwd())
+import utils.exceltools as extool
+import utils.dbtools as db
+def test_01_getquestiondetail_success():
+    e_data = extool.read_excel("data/测谈网接口测试用例.xlsx", "获取问题详情")
+    url = e_data[0][2]
+    head = eval(e_data[0][5])
+    res = requests.get(url=url,headers=head)#head中规定使用json，因此用json而不是data
+    assert res.json()["status"] == 200#状态码
+    assert res.status_code == 200#结果码
+    assert len(res.json()["data"]) != 0
+    
+
+def test_02_getquestiondetail_fail_xiaoshu():
+    e_data = extool.read_excel("data/测谈网接口测试用例.xlsx", "获取问题详情")
+    url = e_data[1][2]
+    head = eval(e_data[1][5])
+    res = requests.get(url=url,headers=head)#head中规定使用json，因此用json而不是data
+    assert res.json()["status"] == 401#状态码
+    assert res.status_code == 200#结果码
+    assert res.json()["msg"] == "请输入正整数"
+
+def test_03_getquestiondetail_fail_fushu():
+    e_data = extool.read_excel("data/测谈网接口测试用例.xlsx", "获取问题详情")
+    url = e_data[2][2]
+    head = eval(e_data[2][5])
+    res = requests.get(url=url,headers=head)#head中规定使用json，因此用json而不是data
+    assert res.json()["status"] == 401#状态码
+    assert res.status_code == 200#结果码
+    assert res.json()["msg"] == "请输入正整数"
+
+def test_04_getquestiondetail_fail_zifu():
+    e_data = extool.read_excel("data/测谈网接口测试用例.xlsx", "获取问题详情")
+    url = e_data[3][2]
+    head = eval(e_data[3][5])
+    res = requests.get(url=url,headers=head)#head中规定使用json，因此用json而不是data
+    assert res.json()["status"] == 401#状态码
+    assert res.status_code == 200#结果码
+    assert res.json()["msg"] == "请输入正整数"
+
+def test_05_getquestiondetail_fail_char():
+    e_data = extool.read_excel("data/测谈网接口测试用例.xlsx", "获取问题详情")
+    url = e_data[4][2]
+    head = eval(e_data[4][5])
+    res = requests.get(url=url,headers=head)#head中规定使用json，因此用json而不是data
+    assert res.json()["status"] == 401#状态码
+    assert res.status_code == 200#结果码
+    assert res.json()["msg"] == "请输入正整数"
+
+def test_06_getquestiondetail_fail_null():
+    e_data = extool.read_excel("data/测谈网接口测试用例.xlsx", "获取问题详情")
+    url = e_data[5][2]
+    head = eval(e_data[5][5])
+    res = requests.get(url=url,headers=head)#head中规定使用json，因此用json而不是data
+    assert res.json()["status"] == 401#状态码
+    assert res.status_code == 200#结果码
+    assert res.json()["msg"] == "请输入正整数"
+
+def test_07_getquestiondetail_fail_notexist():
+    e_data = extool.read_excel("data/测谈网接口测试用例.xlsx", "获取问题详情")
+    url = e_data[6][2]
+    head = eval(e_data[6][5])
+    res = requests.get(url=url,headers=head)#head中规定使用json，因此用json而不是data
+    assert res.json()["status"] == 401#状态码
+    assert res.status_code == 200#结果码
+    assert res.json()["msg"] == "请输入正整数"
---
+++ utils/exceltools.py
@@ -0,0 +1,24 @@
+"""
+    读取excel
+"""
+import os,sys
+sys.path.append(os.getcwd())
+import xlrd
+
+def read_excel(excel_path, sheet_name, skip_first=True):
+    results = []
+    datas = xlrd.open_workbook(excel_path)
+    table = datas.sheet_by_name(sheet_name)
+    if skip_first == True:
+        start_row = 1
+    else:
+        start_row = 0
+
+    # 循环读取每一行的数据
+    for row in range(start_row, table.nrows):
+        results.append(table.row_values(row))
+
+    return results
+
+# if __name__ == "__main__":
+#     print(read_excel("data.xlsx", "hh"))
